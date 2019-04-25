77499bf3da9846b62ba1824b500638fa20a1f783
cumtzwf <cumtzwf@163.com>
2019-04-25 08:35:15 +0800

    修改led模块程序，由原来手动创建设备节点改为加载后自动创建设备节点

--- my_modules/mychar_dev/led_dev/app/led_app.c
+++ my_modules/mychar_dev/led_dev/app/led_app.c
@@ -8,28 +8,28 @@
 
 int main(int argc, char *argv[])
 {
-     int fd;
-     int cmd;
-     
-     if (argc <2 )
-     {
-         printf("please enter the second para!\n");
-         return 0;	
-     }
-     
-     cmd = atoi(argv[1]); 
-     
-     fd = open("/dev/myled",O_RDWR);
-	 if(fd < 0){
+	int fd;
+	int cmd;
+
+	if (argc <2 )
+	{
+		printf("please enter the second para!\n");
+		return 0;
+	}
+
+	cmd = atoi(argv[1]);
+
+	fd = open("/dev/myled",O_RDWR);
+	if(fd < 0){
 		printf("open dev fail!\n"); 
 		exit(1);
-	 }
-     
-     if (cmd == 1)
-         ioctl(fd,LED_ON);
-     else
-         ioctl(fd,LED_OFF);	
+	}
 
+	if (cmd == 1)
+		ioctl(fd,LED_ON);
+	else
+		ioctl(fd,LED_OFF);
+	close(fd);
 
      return 0;
 }
--- my_modules/mychar_dev/led_dev/driver/led_driver.c
+++ my_modules/mychar_dev/led_dev/driver/led_driver.c
@@ -3,17 +3,20 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/io.h>
+#include <linux/device.h>
 
 #include "led.h"
 
 #define LED_GPC0CON	0xE0200060
 #define LED_GPC0DAT	0xE0200064
 
-unsigned int *led_config; 
-unsigned int *led_data; 
+static unsigned int *led_config = NULL;
+static unsigned int *led_data  = NULL;
 
-struct cdev cdev;
-dev_t devno;
+static struct cdev cdev;
+static dev_t devno;
+static struct class *cls;
+static struct device *mydev;
 
 int led_open (struct inode *node, struct file *filp)
 {
@@ -22,6 +25,7 @@ int led_open (struct inode *node, struct file *filp)
 	tmp = readl(led_config) & (~0xff000);
 	writel(tmp | 0x11000,led_config);
 	led_data = ioremap(LED_GPC0DAT,4);
+	printk("Kernel led_open\n");
 	return 0;
 }
 
@@ -54,16 +58,39 @@ const struct file_operations led_fops = {
 
 static int led_init(void)
 {
-	cdev_init(&cdev, &led_fops);
-	alloc_chrdev_region(&devno, 0, 1,"myled");
-	cdev_add(&cdev, devno, 1);
+	int retval = -1;
+	cdev_init(&cdev, &led_fops);// 初始化cdev结构和操纵这个字符设备文件的方法,
+	// 对应： cat /proc/devices 254 myled
+	alloc_chrdev_region(&devno, 0, 1,"myled");//动态获取一个主设备号和次设备号范围
+
+	retval = cdev_add(&cdev, devno, 1);
+	if (retval != 0) {
+		printk("cdev_add character device fail\n");
+	}
+	// 对应： /sys/class/myclass
+	cls = class_create(THIS_MODULE,"myclass");
+	if(IS_ERR(cls)){
+		unregister_chrdev_region(devno,1);
+		return -EBUSY;
+	}
+	// 对应： /sys/class/myclass/myled
+	mydev = device_create(cls,NULL,devno,NULL,"myled");
+	if(IS_ERR(mydev)){
+		class_destroy(cls);
+		unregister_chrdev_region(devno,1);
+		return -EBUSY;
+	}
+
 	return 0;
 }
 
 static void led_exit(void)
 {
 	cdev_del(&cdev);
+	device_destroy(cls,devno);
+	class_destroy(cls);
 	unregister_chrdev_region(devno, 1);
+	printk("led_exit\n");
 }
 
 MODULE_LICENSE("GPL");
